1c532f99f63eaff95d5913e98b0f1dfea1de5c70
Mr.Dominic <ochu.dominic.onah@gmail.com>
2022-08-25 22:22:45 +0100

    feature: the background images

--- src/App.js
+++ src/App.js
@@ -5,12 +5,43 @@ const App = () => {
   return (
     <section className="timer-container">
       <div className="timer">
-        <span className="mdi mdi-calendar-clock timer-icon"></span>
-        <h2>CountDown Timer</h2>
-        <p>
-          Countdown to a really special date. One you could or would ever
-          imagine!
-        </p>
+        <div>
+          <span className="mdi mdi-calendar-clock timer-icon"></span>
+          <h2>CountDown Timer</h2>
+          <p>
+            Countdown to a really special date. One you could or would ever
+            imagine!
+          </p>
+        </div>
+        <div>
+          <section>
+            <p>87</p>
+            <p>
+              <small>Days</small>
+            </p>
+          </section>
+          <span>:</span>
+          <section>
+            <p>87</p>
+            <p>
+              <small>Hours</small>
+            </p>
+          </section>
+          <span>:</span>
+          <section>
+            <p>87</p>
+            <p>
+              <small>Minutes</small>
+            </p>
+          </section>
+          <span>:</span>
+          <section>
+            <p>87</p>
+            <p>
+              <small>Seconds</small>
+            </p>
+          </section>
+        </div>
       </div>
     </section>
   );
