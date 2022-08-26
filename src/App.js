8b807653ab4e37f3a753885e63667e9f88d6fb18
Mr.Dominic <ochu.dominic.onah@gmail.com>
2022-08-26 21:23:54 +0100

    feature: added the days, hrs, mins,secs calc.

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useRef, useState } from "react";
 import "./App.css";
 
 const App = () => {
@@ -6,6 +6,24 @@ const App = () => {
   const [timerHours, setTimerHours] = useState("00");
   const [timerMinutes, setTimerMinutes] = useState("00");
   const [timerSeconds, setTimerSeconds] = useState("00");
+
+  let interval = useRef();
+
+  const starterTimer = () => {
+    const countDownDate = new Date("july 09, 2023 00:00:00").getTime();
+
+    interval = setInterval(() => {
+      const now = new Date().getTime();
+      const distance = countDownDate - now;
+      const days = Math.floor(distance / (1000 * 24 * 60 * 60));
+      const hours = Math.floor(
+        (distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60)
+      );
+      const minutes = Math.floor((distance % (1000 * 60)) / (1000 * 60));
+      const seconds = Math.floor((distance % (1000 * 60)) / 1000);
+    }, 1000);
+  };
+
   return (
     <section className="timer-container">
       <section className="timer">
