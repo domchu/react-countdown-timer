8f67ede8acf557fa8d16690cd0dbd8274a7e75ae
Mr.Dominic <ochu.dominic.onah@gmail.com>
2022-08-26 22:50:22 +0100

    Bug: fixing the seconds timer calc

--- src/App.js
+++ src/App.js
@@ -19,7 +19,7 @@ const App = () => {
       const hours = Math.floor(
         (distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60)
       );
-      const minutes = Math.floor((distance % (1000 * 60)) / (1000 * 60));
+      const minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
       const seconds = Math.floor((distance % (1000 * 60)) / 1000);
 
       if (distance < 0) {
